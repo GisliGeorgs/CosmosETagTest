277e378b76142484c33f6c87fa77c1a20f4856b2
GisliGeorgs <gisligeorgsson@solidclouds.com>
2022-10-13 08:05:02 +0000

    Init

---
+++ Db.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CosmosEtag;
+
+public class Db : DbContext
+{
+    public DbSet<UseETagEntity> UseEtagEntities => Set<UseETagEntity>();
+    public DbSet<IsETagEntity> IsETagEntities => Set<IsETagEntity>();
+    public DbSet<NoETagEntity> NoETagEntities => Set<NoETagEntity>();
+
+    public Db(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        var useEntity = modelBuilder.Entity<UseETagEntity>()
+            .ToContainer(nameof(UseETagEntity));
+        useEntity.UseETagConcurrency();
+        useEntity.HasPartitionKey(x => x.PartitionKey);
+        useEntity.HasKey(x => x.Id);
+
+        var isEntity = modelBuilder.Entity<IsETagEntity>()
+            .ToContainer(nameof(IsETagEntity));
+        isEntity.Property(x => x._etag).IsETagConcurrency();
+        isEntity.HasPartitionKey(x => x.PartitionKey);
+        isEntity.HasKey(x => x.Id);
+
+        var noEntity = modelBuilder.Entity<NoETagEntity>()
+            .ToContainer(nameof(NoETagEntity));
+        noEntity.HasPartitionKey(x => x.PartitionKey);
+        noEntity.HasKey(x => x.Id);
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
+
+public abstract class Entity
+{
+    public string PartitionKey { get; set; } = string.Empty;
+    public int Id { get; set; }
+
+    public string PropertyToChange { get; set; } = string.Empty;
+}
+public class UseETagEntity : Entity
+{
+}
+
+public class IsETagEntity : Entity
+{
+    public string _etag { get; set; } = string.Empty;
+}
+
+public class NoETagEntity : Entity
+{
+}
---
+++ Program.cs
@@ -0,0 +1,153 @@
+﻿using CosmosEtag;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+Console.WriteLine("Hello, World!");
+
+var services = new ServiceCollection();
+
+services.AddCosmos<Db>(
+    "AccountEndpoint=https://localhost:8081/;AccountKey=C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==",
+    "ETagThing");
+
+var serviceProvider = services.BuildServiceProvider();
+
+Console.WriteLine("Setting up");
+
+using (var scope = serviceProvider.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<Db>();
+    await db.Database.EnsureDeletedAsync().ConfigureAwait(false);
+    await db.Database.EnsureCreatedAsync().ConfigureAwait(false);
+
+    db.UseEtagEntities.Add(new()
+    {
+        Id = 1,
+        PartitionKey = "Use",
+        PropertyToChange = "Unmodified"
+    });
+
+    db.IsETagEntities.Add(new()
+    {
+        Id = 2,
+        PartitionKey = "Is",
+        PropertyToChange = "Unmodified"
+    });
+
+    db.NoETagEntities.Add(new()
+    {
+        Id = 3,
+        PartitionKey = "No",
+        PropertyToChange = "Unmodified"
+    });
+
+    await db.SaveChangesAsync().ConfigureAwait(false);
+}
+
+Console.WriteLine("Set up, starting");
+
+// On a SaveChangesAsync breakpoint, edit the relevant entity
+using (var scope = serviceProvider.CreateScope())
+{
+    Console.WriteLine("UseETag - should throw, but doesn't");
+    var scopeDb = scope.ServiceProvider.GetRequiredService<Db>();
+
+    var useEntity = await scopeDb.UseEtagEntities
+        .WithPartitionKey("Use")
+        .FirstAsync(x => x.Id == 1)
+        .ConfigureAwait(false);
+
+    useEntity.PropertyToChange = "Modified in scope";
+
+    await ModifyInDifferentScope<UseETagEntity>("Use", 1).ConfigureAwait(false);
+
+    // Expected behaviour: Throw
+    // Actual: Doesn't throw
+    var threw = false;
+    try
+    {
+        await scopeDb.SaveChangesAsync().ConfigureAwait(false);
+    }
+    catch (DbUpdateConcurrencyException)
+    {
+        threw = true;
+    }
+
+    Console.WriteLine($"UseETag threw: {threw}");
+}
+
+using (var scope = serviceProvider.CreateScope())
+{
+    Console.WriteLine("IsETag - should throw, does");
+    var scopeDb = scope.ServiceProvider.GetRequiredService<Db>();
+
+    var isEntity = await scopeDb.IsETagEntities
+        .WithPartitionKey("Is")
+        .FirstAsync(x => x.Id == 2)
+        .ConfigureAwait(false);
+
+    isEntity.PropertyToChange = "Modified in scope";
+
+    await ModifyInDifferentScope<IsETagEntity>("Is", 2).ConfigureAwait(false);
+
+    // Expected behaviour: Throws
+    // Actual: Throws
+    var threw = false;
+    try
+    {
+        await scopeDb.SaveChangesAsync().ConfigureAwait(false);
+    }
+    catch (DbUpdateConcurrencyException)
+    {
+        threw = true;
+    }
+
+    Console.WriteLine($"IsETag threw: {threw}");
+}
+
+using (var scope = serviceProvider.CreateScope())
+{
+    Console.WriteLine("NoETag - shouldn't throw, doesn't");
+    var scopeDb = scope.ServiceProvider.GetRequiredService<Db>();
+
+    var isEntity = await scopeDb.NoETagEntities
+        .WithPartitionKey("No")
+        .FirstAsync(x => x.Id == 3)
+        .ConfigureAwait(false);
+
+    isEntity.PropertyToChange = "Modified in scope";
+
+    await ModifyInDifferentScope<NoETagEntity>("No", 3).ConfigureAwait(false);
+
+    // Optional
+    // Breakpoint 3 - Before continuing, edit NoETagEntity with id 3
+    // Expected behaviour: Doesn't throw
+    // Actual: Doesn't throw
+    var threw = false;
+    try
+    {
+        await scopeDb.SaveChangesAsync().ConfigureAwait(false);
+    }
+    catch (DbUpdateConcurrencyException)
+    {
+        threw = true;
+    }
+
+    Console.WriteLine($"NoETag threw: {threw}");
+}
+
+async Task ModifyInDifferentScope<T>(string partitionKey, int id) where T : Entity
+{
+    var differentScope = serviceProvider.CreateScope();
+
+    var differentScopeDb = differentScope.ServiceProvider.GetRequiredService<Db>();
+
+    var entity = await differentScopeDb.Set<T>()
+        .WithPartitionKey(partitionKey)
+        .FirstAsync(x => x.Id == id)
+        .ConfigureAwait(false);
+
+    entity.PropertyToChange = "Modified in different scope";
+
+    await differentScopeDb.SaveChangesAsync().ConfigureAwait(false);
+}
